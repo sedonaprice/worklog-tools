c40b0cbb97909eefc5a66563bc2b5b8eab0febe4
Sedona Price <sedona.price@gmail.com>
2021-12-06 15:01:59 +0100

    Update worklog.py

--- worklog.py
+++ worklog.py
@@ -1832,9 +1832,13 @@ def _bib_fixup_author (text):
     return rest + ' ' + surname.replace (' ', '_')
 
 
-_bib_months = {'jan': '01', 'feb': '02', 'mar': '03', 'apr': '04',
-               'may': '05',  'jun': '06',  'jul': '07',  'aug': '08',
-               'sep': '09',  'oct': '10',  'nov': '11',  'dec': '12'}
+# _bib_months = {'jan': '01', 'feb': '02', 'mar': '03', 'apr': '04',
+#                'may': '05',  'jun': '06',  'jul': '07',  'aug': '08',
+#                'sep': '09',  'oct': '10',  'nov': '11',  'dec': '12'}
+
+_bib_months = {'january': '01', 'february': '02', 'march': '03', 'april': '04',
+               'may': '05',  'june': '06',  'julu': '07',  'august': '08',
+               'september': '09',  'october': '10',  'november': '11',  'december': '12'}
 
 _bib_journals = {'\\aap': 'A&Ap', '\\aj': 'AJ', '\\apj': 'ApJ',
                  '\\apjl': 'ApJL', '\\apjs': 'ApJS', '\\araa': 'ARA&A',
